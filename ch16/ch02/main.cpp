16345e4d76501d4fe954ce470dd17e6078ddd95a
jinjianxin <jinjianxinjin@gmail.com>
2012-08-28 13:18:20 +0800

    modified:   ch16/ch02/main.cpp

--- ch16/ch02/main.cpp
+++ ch16/ch02/main.cpp
@@ -2,9 +2,10 @@
 
 using namespace std;
 
-template<class T,int size=100>
+template<class T,int size=10>
 class Array
 {
+
 	T array[size];
 	public:
 	T& operator [] (int index)
@@ -21,25 +22,20 @@ class Array
 class Number
 {
 	public:
-	Number(float ff=0.1f):f(ff) {}
+//	explicit Number(float ff=0.1f):f(ff) {cout<<"number init"<<endl;}
+    Number(float ff=0.1f):f(ff) {cout<<"number init float"<<endl;} // 此处不能加上explicit关键字，否则无法转换
+//	Number(int i = 0):ii(i)  {cout<<"number init int"<<endl;};
+
 	Number& operator = (const  Number& number)  //此处必须使用const关键字，否则无法重载该运算符
 	{
-		cout<<number.f<<endl;
-		f = number.f;
+		f =number.f;
 		return *this;
 	}
-/*	
-	Number& operator = (const float i)
-	{
-		f = i;
-		return *this;
-	}
-*/
 	float f;
 	operator float() const {return f;}
 };
 
-template<class T,int size=10>
+template<class T,int size=5> //只为一种类型生成一个实例
 class Holder
 {
 	Array<T,size> *np;
@@ -49,11 +45,12 @@ class Holder
 	{
 		if(np == 0)
 		{
-			np = new Array<T,size>;
+			np = new Array<T,size>;  //懒惰初始化，只有等调用时在初始化，
 		}
-
-		np->operator[] (i); //此处调用Array的[] 运算符
+	
+		np->operator[] (i); //此处调用Array的[] 运算符	
 	}
+	
 
 	int length () const {return size;}
 	~Holder() {delete np;};
@@ -62,9 +59,9 @@ class Holder
 int main()
 {
 	Holder<Number> is;
-	for(int i=0;i<5;i++)
+	for(int i=0;i<1;i++)
 	{	
-		is[i] = i; //此处先调用Holder的[]运算符，在调用 Number的= 运算符
+		is[i] =i; // 编译器将i隐式转化为number的对象  调用Holder的[]运算符，在调用Number的= 运算符  讲i 赋值给is[i]
 	}
 
 	return 0;
